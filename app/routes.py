08dea3c987a4d98e4efff201f4fc7ed044de56f7
Egor Biriukov <birykovegor@gmail.com>
2016-12-15 17:21:46 +0300

    POST endpoint for Notes resource

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
 import views
 
 def setup_routes(app):
-    app.router.add_get('/api/v1/notes', views.notes_index)
+    app.router.add_route('*', '/api/notes', views.NoteView)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,31 @@
+from datetime import datetime
 import db
+import sqlalchemy as sa
 from aiohttp import web
 
+from helpers import json_response
 
-async def notes_index(request):
-    async with request.app['db'].acquire() as conn:
-        cursor = await conn.execute(db.note.select())
-        records = await cursor.fetchall()
-        notes = [dict(n) for n in records]
-        return web.json_response(data={'notes': notes})
+
+class NoteView(web.View):
+
+    def __aquire_db(self):
+        return self.request.app['db'].acquire()
+
+    async def get_post_params(self):
+        params = await self.request.json()
+        time_ = datetime.now()
+        params['updated_at'] = params['created_at'] = time_
+        return params
+
+    async def get(self):
+        async with self.__aquire_db() as conn:
+            cursor = await conn.execute(db.note.select())
+            records = await cursor.fetchall()
+            notes = [dict(n) for n in records]
+            return json_response(data={'notes': notes})
+
+    async def post(self):
+        params = await self.get_post_params()
+        async with self.__aquire_db() as conn:
+            cursor = await conn.execute(db.note.insert(params))
+        return web.json_response(data="ok")
